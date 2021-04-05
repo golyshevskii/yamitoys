020f0a90602aa6e2588b7971d49683dfae456a72
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-05 16:22:34 +0300

    Update urls.py

--- settings+urls/urls.py
+++ settings+urls/urls.py
@@ -1,29 +1,17 @@
-"""pupa URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+# список путей, ведущих к приложениям сайта
+# list of paths leading to site applications
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^toys/', include('toys.urls')),
-    re_path(r'^cart/', include('cart.urls')),
+    re_path(r'^admin/', admin.site.urls),     # путь к страничке администрации сайта; path to the site administration page
+    re_path(r'^toys/', include('toys.urls')), # путь к приложению toys; path to toys app
+    re_path(r'^cart/', include('cart.urls')), # путь к приложению cart; path to cart app
 ]
 
+# использование статических файлов(изображения для игрушек), если DEBUG = True
+# use static files (images for toys) if DEBUG = True
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
