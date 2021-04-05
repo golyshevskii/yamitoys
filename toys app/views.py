d8b2fddbeac1643f1241e8fa0df87f733929768a
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-05 21:33:13 +0300

    Update views.py

--- toys app/views.py
+++ toys app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from toys.models import Toy, Typ
-from cart.forms import CartAddProductForm
+from cart.forms import CartAddProductForm # подключение формы для добавления игрушки в корзину; connecting a form to add a toy to the cart
 
 
+ # метод вывода информации о доступных игрушках; method for displaying information about available toys
 def toy_list(request, typ_slug=None):
     toy = Toy.objects.filter(available=True)
     types = Typ.objects.all()
@@ -14,6 +15,7 @@ def toy_list(request, typ_slug=None):
     return render(request, 'toys/toys1.html', context)
 
 
+# метод отображения сведений об игрушке; method for displaying toy details
 def toy_detail(request, id, slug):
     toy = get_object_or_404(Toy, id=id, slug=slug, available=True)
     cart_toy_form = CartAddProductForm()
