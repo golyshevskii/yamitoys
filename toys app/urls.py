4ac1d17f3f83629b5f418dcbeb041bb0ee0fb485
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-05 21:28:51 +0300

    Update urls.py

--- toys app/urls.py
+++ toys app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
 from toys.views import toy_list, toy_detail
 
-app_name = 'toys'
+app_name = 'toys' # имя приложения
 
+# пути к методам из файла views
+# paths to methods from the views file
 urlpatterns = [
     re_path(r'^$', toy_list, name='all_toys'),
     re_path(r'^(?P<typ_slug>[-\w]+)/$', toy_list, name='toys_by_type'),
