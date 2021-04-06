3186b1fd8edc1f9f75374bb5349ac33b27ef8476
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-06 09:17:05 +0300

    Update urls.py

--- cart app/urls.py
+++ cart app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
 from .views import cart_detail, cart_add, cart_remove
 
-app_name = 'cart'
+app_name = 'cart' # имя приложения
 
+# маршрутизаторы
+# routers
 urlpatterns = [
     re_path(r'^$', cart_detail, name='cart_detail'),
     re_path(r'^add/(?P<toy_id>\d+)/$', cart_add, name='cart_add'),
