68b86c7a9b5c59740a7f1fa0bd6babcc76a7a1c9
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-06 09:14:36 +0300

    Update forms.py

--- cart app/forms.py
+++ cart app/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 
+
+# количесво возможного выбора игрушек при помещении игрушки в корзину
+# number of possible choice of toys when placing a toy in the cart
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
+# форма; form
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
