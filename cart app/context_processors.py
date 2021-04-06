758bbfaea2f25eb898b26b88a84f8b8598153cb3
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-06 09:36:25 +0300

    Update context_processors.py

--- cart app/context_processors.py
+++ cart app/context_processors.py
@@ -1,6 +1,8 @@
 from .cart import Cart
 
 
+# контекстный метод принимающий запрос и возвращающий словарь
+# context method accepting a request and returning a dictionary
 def cart(request):
     context = {'cart': Cart(request)}
     return context
