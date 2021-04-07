1aaf563e4ef61c54687c76d3339ab870db2b4b8b
Slava Golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2021-04-07 15:09:33 +0300

    Update urls.py

--- settings+urls/urls.py
+++ settings+urls/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls.static import static
 # список путей, ведущих к приложениям сайта
 # list of paths leading to site applications
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),     # путь к страничке администрации сайта; path to the site administration page
-    re_path(r'^toys/', include('toys.urls')), # путь к приложению toys; path to toys app
-    re_path(r'^cart/', include('cart.urls')), # путь к приложению cart; path to cart app
+    re_path(r'^admin/', admin.site.urls),         # путь к страничке администрации сайта; path to the site administration page
+    re_path(r'^toys/', include('toys.urls')),     # путь к приложению toys; path to toys app
+    re_path(r'^cart/', include('cart.urls')),     # путь к приложению cart; path to cart app
+    re_path(r'^orders/', include('orders.urls')), # путь к приложению orders; path to orders app
 ]
 
 # использование статических файлов(изображения для игрушек), если DEBUG = True
